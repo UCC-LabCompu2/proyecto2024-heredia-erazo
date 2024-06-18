ec9a39d448b3f8aaab98724a072c6333c9249832
Ivan <163861405+ivanjalid1@users.noreply.github.com>
2024-06-18 02:54:36 -0300

    Actualización
    Se agregaron/mejoraron funcionalidades al js y se agregó una página para el ticket.

--- scripts.js
+++ scripts.js
@@ -1,34 +1,39 @@
-/*
- * Descripción: Valida si un campo de un form está vacío
+/**
+ * Descripción: Valida si un campo de un form está vacío y si es correcto
  * @method validarCampo
- * @param {String} campo - ID del elemento HTML del campo a validar
+ * @param campo
  * @param {String} nombreCampo - Nombre del campo a validar (para mensajes de alerta)
  * @return {boolean} - True si el campo está vacío, false si no lo está
  */
 let validarCampo = (campo, nombreCampo) => {
-    if(campo.value === ""){ // Se verifica si el campo está vacío
-        alert("Ingrese su " + nombreCampo);
+    const caracteres = /^[A-Za-zñ\s]+$/;
+    if (campo.value === "") {
+        alert(`Ingrese su ${nombreCampo}.`);
+        campo.value = "";
+        return false;
+    } else if (!caracteres.test(campo.value)) {
+        alert(`Ingrese un ${nombreCampo} válido (solo letras).`);
         campo.value = "";
         return false;
     }
     return true;
 }
-/*
+/**
  * Descripción: Valida si se selecciona una opción de género
  * @method validarGenero
  * @return {boolean} - True si se selecciona una opción de género, false si no se selecciona
  */
 let validarGenero = () => {
-    let generos = document.getElementsByName("genero");
-    for(let i = 0; i < generos.length; i++) {
-        if(generos[i].checked) { // si existe alguna opción seleccionada está ok
+    let generos = document.getElementsByName('genero');
+    for (let i = 0; i < generos.length; i++) {
+        if (generos[i].checked) { // si existe alguna opción seleccionada está ok
             return true;
         }
     }
-    alert("ERROR / Seleccione un género");
+    alert("Seleccione un género.");
     return false;
 }
-/*
+/**
  * Descripción: Valida si un mail tiene el formato válido
  * @method validarEmail
  * @param {String} email - Mail a validar
@@ -36,11 +41,11 @@ let validarGenero = () => {
  */
 
 let validarEmail = (email) => {
-    const emailPatron = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; // Se define el patrón del mail
-    return emailPatron.test(email); // Prueba si el mail concuerda con el patrón
+    const emailPatron = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailPatron.test(email);
 }
 
-/*
+/**
  * Descripción: Valida si un formulario tiene todos los campos completados
  * @method validarForm
  * @return {boolean} esValido - Devuelve true si todos los campos están completados y validados, false si no lo están
@@ -48,71 +53,85 @@ let validarEmail = (email) => {
 
 
 const validarForm = () => {
-    let nombre = document.getElementById("nombre");
-    let apellido = document.getElementById("apellido");
-    let edad = document.getElementById("edad");
-    let email = document.getElementById("email");
-    let comentario = document.getElementById("comentario");
+    let nombre = document.getElementById('nombre');
+    let apellido = document.getElementById('apellido');
+    let edad = document.getElementById('edad');
+    let email = document.getElementById('email');
+    let comentario = document.getElementById('comentario');
 
     let esValido = true;
 
-    if(!validarCampo(nombre, "nombre")){
+    if (!validarCampo(nombre, "nombre")) {
         esValido = false;
     }
-    if(!validarCampo(apellido, "apellido")){
+    if (!validarCampo(apellido, "apellido")) {
         esValido = false;
     }
-
-    if (!validarGenero()) {
-        esValido = false;
-    }
-
     if (edad.value === "" || edad.value < 0 || edad.value > 200) {
-        alert("ERROR / Ingrese una edad válida entre 0 y 200");
+        alert("Ingrese una edad válida entre 0 y 200.");
         edad.value = "";
         esValido = false;
     }
-
+    if (!validarGenero()) {
+        esValido = false;
+    }
     if (!validarEmail(email.value)) {
-        alert("ERROR / Ingrese un email válido");
+        alert("Ingrese un email válido.");
         email.value = "";
         esValido = false;
     }
-
-    if (!validarCampo(comentario, "comentario")){
+    if (comentario.value === "") {
+        alert("Ingrese un comentario.");
         esValido = false;
     }
-
+    if (esValido) {
+        alert("Formulario enviado con éxito.");
+    }
     return esValido;
 }
 
-// ...
-
-
+/**
+ * Descripción: Añade un producto al carrito de compras
+ * @method anadirProducto
+ * @param {String} nombreProducto - Nombre del producto a añadir
+ * @param {number} precio - Precio del producto a añadir
+ */
 let anadirProducto = (nombreProducto, precio) => {
     let carro = JSON.parse(localStorage.getItem('carro')) || [];
 
-    carro.push({ nombreProducto, precio });
+    carro.push({nombreProducto, precio});
 
     localStorage.setItem('carro', JSON.stringify(carro));
 
-    alert(nombreProducto + ' se agregó al carrito.');
+    alert(`${nombreProducto} se agregó al carrito.`);
 }
-
+/**
+ * Descripción: Elimina un producto del carrito
+ * @method eliminarProducto
+ * @param {number} index - Índice del producto a eliminar en el carrito
+ */
 let eliminarProducto = (index) => {
-    let carro = JSON.parse(localStorage.getItem('carro')) || [];
+    const carro = JSON.parse(localStorage.getItem('carro')) || [];
 
     carro.splice(index, 1);
 
     localStorage.setItem('carro', JSON.stringify(carro));
 
     compraLocal();
-    dibujarTicket();
-}
 
+    if (carro.length === 0) {
+        setTimeout(() => {
+            alert("Se eliminaron todos los productos del carrito, seleccione nuevos productos.");
+        }, 500);
+    }
+}
+/**
+ * Descripción: Muestra el contenido del carrito de compras
+ * @method compraLocal
+ */
 let compraLocal = () => {
-    let carro = JSON.parse(localStorage.getItem('carro')) || [];
-    let carroInfo = document.getElementById('carro-info');
+    const carro = JSON.parse(localStorage.getItem('carro')) || [];
+    const carroInfo = document.getElementById('carro-info');
     let total = 0;
 
     carroInfo.innerHTML = ''; // Limpia el contenido previo, no sacar
@@ -125,72 +144,102 @@ let compraLocal = () => {
         div.innerHTML = `
             <p>${producto.nombreProducto}</p>
             <p>Precio: $${producto.precio}</p>
-            <button class="button" onclick="eliminarProducto(${i})">Eliminar producto</button>
+            <button class="button-eliminar" onclick="eliminarProducto(${i})">Eliminar producto</button>
         `;
         carroInfo.appendChild(div);
 
         total += producto.precio;
     }
 
-    document.getElementById('precio-total').textContent = 'Total: $' + total;
+    document.getElementById('precio-total').textContent = `Total: $${total}`;
 }
 
-
-    /*
-    * Descripcion: valida la busqueda si se ingresan letras
-    * method: busqueda
-    * pa
-    */
-
+/**
+ * Descripción: Valida que la búsqueda concuerde con los productos y redirige a la página de productos si existe el producto
+ * @method busquedaCorrecta
+ * @return {boolean} - Devuelve true si la búsqueda es válida, false si no lo es
+ */
 let busquedaCorrecta = () => {
-    let busqueda = document.getElementById("buscar").value;
-    let caracteres = /^[A-Za-z]+$/;
-    if (caracteres.test(busqueda)) {
+    let busqueda = document.getElementById('buscar');
+    let busquedaMin = busqueda.value.toLowerCase();
+    if (busquedaMin === "buzo" || busquedaMin === "remera") {
         window.location.href = 'productos.html';
         return true;
     } else {
-        alert('ERROR / ingrese valor valido (solo letras)');
-        document.getElementById("buscar").value = "";
+        alert("El producto que deseas buscar no existe, ingrese uno correcto.");
+        busqueda.value = "";
         return false;
     }
 }
-
+/**
+ * Descripción: Dibuja un ticket de compra en el canvas con los productos del carrito
+ * @method dibujarTicket
+ */
 let dibujarTicket = () => {
-    const productos = JSON.parse(localStorage.getItem('carro')) || [];
+    const productos = JSON.parse(localStorage.getItem('productosTicket')) || [];
     const canvas = document.getElementById('ticketCanvas');
     const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpia canvas
-
-    ctx.font = '16px Outfit';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'left';
 
     let x = 30;
     let y = 30;
-    let total= 0;
+    let total = 0;
 
-    ctx.fillText('Ticket de Compra', x+70, y);
-    y += 30;
+    const alturaLinea = 30; // espacio entre cada línea
+    const alturaEncabezado = 60;  // linea superior -> título -> espacio
+    const alturaFooter = 55;  // linea -> total -> espacio (25)
 
+    let alturaCanvas = alturaEncabezado + (productos.length * alturaLinea) + alturaFooter;
+
+    canvas.height = alturaCanvas;
+
+    // Limpiar el canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.font = '18px Outfit';
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'center';
+
+    // Encabezado
+    ctx.fillStyle = '#f0f0f0';
+    ctx.fillRect(0, 0, canvas.width, alturaEncabezado - 20);
+
+    ctx.fillStyle = 'black';
+    ctx.fillText("Ticket de Compra", canvas.width / 2, y);
+    y += alturaLinea;
+
+    ctx.textAlign = 'left';
+
+    // Productos
     for (let i = 0; i < productos.length; i++) {
         let producto = productos[i];
         ctx.fillText(`${producto.nombreProducto}: $${producto.precio.toFixed(2)}`, x, y);
         total += producto.precio;
-        y += 30;
+        y += alturaLinea;
     }
 
-    // Dibuja línea en el medio
+    // Línea en el medio
     ctx.beginPath();
     ctx.moveTo(10, y);
     ctx.lineTo(390, y);
+    ctx.strokeStyle = 'black';
     ctx.stroke();
-    y += 25;
+    y += alturaLinea;
 
-    ctx.fillText('Total: $' + total.toFixed(2), x, y);
+    ctx.fillText(`Total: $${total.toFixed(2)}`, x, y);
 }
 
-let comprar = () => {
-    const canvas = document.getElementById('ticketCanvas');
-    canvas.style.display = 'block'; // Mostrar el canvas
-    dibujarTicket();
+/**
+ * Descripción: Verifica si hay productos en el carrito. Si los hay, muestra el ticket de compra.
+ * @method comprar
+ */
+let emitirTicket = () => {
+    const productos = JSON.parse(localStorage.getItem('carro')) || [];
+    if (productos.length === 0) {
+        alert("No hay productos en el carrito, ingrese productos para comprar.")
+        window.location.href = 'productos.html';
+    } else {
+        localStorage.setItem('productosTicket', JSON.stringify(productos));
+        alert("La compra se realizó correctamente.");
+        window.location.href = 'ticket.html';
+    }
 }
